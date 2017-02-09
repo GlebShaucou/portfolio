df1b57d1e974a3dad8aa03638b164926960eff96
GlebShaucou <shevcov.gleb@gmail.com>
2017-02-09 20:55:39 +0300

    projects modifications

--- projects/fcc/calculator/js/script.js
+++ projects/fcc/calculator/js/script.js
@@ -1,80 +1,15 @@
-var str = "";
-var a = 0; // переменная предотвращающая многократное введение точек
-var b = 0; //переменная предотвращения введения нескольких арифметических знаков подряд 
+(function() {
+    const calculator = document.querySelector('#calculator');
+    const display = document.querySelector('#display');
+    
+    calculator.addEventListener('click', function(event) {
+        
+        if (event.target.classList.contains('num-button')) {
+            display.textContent += event.target.textContent;    
+        }
 
-function dispNumOp(sym) {
-    if(isNaN(sym)) {
-      if(b == 0) {
-        if(str.length > 0) {
-          str = str + sym;
-          $(".disp").html(str);
-          a = 0;
-          b = 1; 
-        }        
-      }      
-    } else {
-      str = str + sym;
-      $(".disp").html(str);
-      b = 0;
-    }    
-}
-
-function zeroNum() {
-  if(str.length == 0) {
-    str = "0" + ".";
-    $(".disp").html(str);
-    a = 1;
-  } else {
-    str = str + "0";
-    $(".disp").html(str);
-  }
-}
-
-function point() {
-  if(a == 0) { 
-    if(str.length == 0) {
-       str = "0" + ".";
-       $(".disp").html(str);
-       a = 1;
-    }else {
-       str = str + ".";
-       $(".disp").html(str);
-       a = 1;
-    }   
-  }
-}
-
-function minSlice() {
-  str = str.slice(0, -1);
-  if(str.length == 0) {
-    $(".disp").html("0")
-  } else {
-    $(".disp").html(str);
-  }
-}
-
-function strSlice() {  
-  if(str.length > 0) {
-    if(str[str.length - 1] == ".") {
-      a = 0;
-      minSlice();
-    } else {
-      minSlice();
-    }    
-  }
-}
-
-function result() {
-  res = eval(str).toPrecision(2);
-  $(".disp").html(res);
-  str = "";
-  a = 0;
-  b = 0;
-}
-
-function clearDisp() {
-  str = "";
-  a = 0;
-  b = 0;
-  $(".disp").html("0");
-}
+        if (event.target.textContent === 'Clear') {
+            display.textContent = '';    
+        }
+    }); 
+})();
